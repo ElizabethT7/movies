3c66977c8c59bf517bb3ff6c1d6706e201ac2871
ElizabethT7 <tymofieievai@gmail.com>
2023-06-20 16:58:07 +0300

    feat: add search fild and preloader

--- src/layout/main/index.jsx
+++ src/layout/main/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Movies from '../../components/Movies';
+import Preloader from '../../components/Preloader';
+import Search from '../../components/Search';
 
 class Main extends React.Component {
   state = {
@@ -16,11 +18,15 @@ class Main extends React.Component {
     const { movies } = this.state;
     return (
       <main className="container content">
+        <Search />
         {
           movies.length ? 
           <Movies movies={this.state.movies} />
           :
-          <h3>Loading...</h3>
+          <div className="center">
+            <Preloader />
+            <h3>Loading...</h3>
+          </div>
         }
         
       </main>
