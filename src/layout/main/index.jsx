eb2797ad2bcd28ebd033444c83d48faf5f7f020a
ElizabethT7 <tymofieievai@gmail.com>
2023-06-22 20:23:58 +0300

    feat: add catch error

--- src/layout/main/index.jsx
+++ src/layout/main/index.jsx
@@ -11,20 +11,28 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
+    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (searchValue, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${searchValue}${
+      `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${searchValue}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
