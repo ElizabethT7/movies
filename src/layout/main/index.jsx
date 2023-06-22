9859f69b9591e29c518bdfaf048080c0dbbbbe31
ElizabethT7 <tymofieievai@gmail.com>
2023-06-22 19:08:47 +0300

    feat: add movie/series filter

--- src/layout/main/index.jsx
+++ src/layout/main/index.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import Movies from '../../components/Movies';
-import Preloader from '../../components/Preloader';
+import Movies from '../../components/card-list';
+import Preloader from '../../components/preloader';
 import Search from '../../components/search';
 
 class Main extends React.Component {
   state = {
     movies: [],
+    loading: true,
   }
 
   componentDidMount() {
     fetch('http://www.omdbapi.com/?i=tt3896198&apikey=36fd8c75&s=matrix')
       .then(response => response.json())
-      .then(data => this.setState({movies: data.Search}))
+      .then(data => this.setState({movies: data.Search, loading: false}))
   }
 
-  searchMovies = (searchValue) => {
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=36fd8c75&s=${searchValue}`)
+  searchMovies = (searchValue, type = 'all') => {
+    this.setState({loading: true})
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=36fd8c75&s=${searchValue}${type !== 'all' ? `&type=${type}` : ''}`)
       .then(response => response.json())
-      .then(data => this.setState({movies: data.Search}))
+      .then(data => this.setState({movies: data.Search, loading: false}))
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
     return (
       <main className="container content">
         <Search searchMovies={this.searchMovies} />
         {
-          movies.length ? 
-          <Movies movies={this.state.movies} />
+          !loading ? 
+          <Movies movies={movies} />
           :
           <div className="center">
             <Preloader />
