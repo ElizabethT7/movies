c07223b362a3bb8cd4b654e558aae52c2dd4c5be
ElizabethT7 <tymofieievai@gmail.com>
2023-06-22 19:33:19 +0300

    feat: add api key const

--- src/layout/main/index.jsx
+++ src/layout/main/index.jsx
@@ -1,44 +1,47 @@
-import React from 'react';
-import Movies from '../../components/card-list';
-import Preloader from '../../components/preloader';
-import Search from '../../components/search';
+import React from "react";
+import Movies from "../../components/card-list";
+import Preloader from "../../components/preloader";
+import Search from "../../components/search";
 
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 class Main extends React.Component {
   state = {
     movies: [],
     loading: true,
-  }
+  };
 
   componentDidMount() {
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=36fd8c75&s=matrix')
-      .then(response => response.json())
-      .then(data => this.setState({movies: data.Search, loading: false}))
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
 
-  searchMovies = (searchValue, type = 'all') => {
-    this.setState({loading: true})
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=36fd8c75&s=${searchValue}${type !== 'all' ? `&type=${type}` : ''}`)
-      .then(response => response.json())
-      .then(data => this.setState({movies: data.Search, loading: false}))
-  }
+  searchMovies = (searchValue, type = "all") => {
+    this.setState({ loading: true });
+    fetch(
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${searchValue}${
+        type !== "all" ? `&type=${type}` : ""
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
+  };
 
   render() {
     const { movies, loading } = this.state;
     return (
       <main className="container content">
         <Search searchMovies={this.searchMovies} />
-        {
-          !loading ? 
+        {!loading ? (
           <Movies movies={movies} />
-          :
+        ) : (
           <div className="center">
             <Preloader />
             <h3>Loading...</h3>
           </div>
-        }
-        
+        )}
       </main>
-    )
+    );
   }
 }
 
